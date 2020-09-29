a125ecb039b8f4239530952e9ccf112a30275db2
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-09-29 18:11:13 +0200

    aisnd

--- test/StoryGraph/StoryGraph.test.ts
+++ test/StoryGraph/StoryGraph.test.ts
@@ -1,19 +1,28 @@
-import { expect } from "chai";
+import { assert } from "chai";
 import StoryGraph from "../../src/index";
 
 describe('StoryGraph', () => {
     describe('constructor', () => {
-        it('should instantiate');
+        it('should instantiate', () => {
+            let story = new StoryGraph();
+            
+            assert.exists(story, 'instantiated');
+        });
+        it('should accept a template graph');
     });
+    
     describe('.connect', () => {
         it('should connect two nodes in a graph');
     });
+    
     describe('.makeGraph', () => {
         it('should construct a new graph');
     });
+    
     describe('.merge', () => {
         it('should merge two graphs');
     });
+    
     describe('.flatten', () => {
         it('should flatten nested graphs');
     });

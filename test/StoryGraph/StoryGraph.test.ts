240a6859f3d9aaf33f224a872e8f34d411042fd5
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-09-29 17:21:52 +0200

    add unit tests

---
+++ test/StoryGraph/StoryGraph.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import StoryGraph from "../../src/index";
+
+describe('Hello World!', () => {
+    it('should say hello', () => {
+        expect("Hello").to.equal("Hello");
+    });
+});

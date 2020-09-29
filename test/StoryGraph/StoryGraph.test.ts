b8f72d42939909e09bae0e010fa9653c240ee532
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-09-29 17:39:39 +0200

    update unit test

--- test/StoryGraph/StoryGraph.test.ts
+++ test/StoryGraph/StoryGraph.test.ts
@@ -1,8 +1,20 @@
 import { expect } from "chai";
 import StoryGraph from "../../src/index";
 
-describe('Hello World!', () => {
-    it('should say hello', () => {
-        expect("Hello").to.equal("Hello");
+describe('StoryGraph', () => {
+    describe('constructor', () => {
+        it('should instantiate');
+    });
+    describe('.connect', () => {
+        it('should connect two nodes in a graph');
+    });
+    describe('.makeGraph', () => {
+        it('should construct a new graph');
+    });
+    describe('.merge', () => {
+        it('should merge two graphs');
+    });
+    describe('.flatten', () => {
+        it('should flatten nested graphs');
     });
 });

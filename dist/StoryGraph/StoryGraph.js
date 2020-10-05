58be8d30011523ee7c95c021f97d9e1e7d45b168
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-10-05 08:49:35 +0200

    update dist

--- dist/StoryGraph/StoryGraph.js
+++ dist/StoryGraph/StoryGraph.js
@@ -8,42 +8,149 @@ var StoryGraph = /** @class */ (function () {
     /**
      *
      */
-    function StoryGraph() {
+    function StoryGraph(nodes, edges) {
+        this.nodes = nodes || [];
+        this.edges = edges || [];
     }
     /**
-     * @param connections
-     * @param graph
+     * @param node
      * @return
      */
-    StoryGraph.prototype.connect = function (connections, graph) {
+    StoryGraph.prototype.addNode = function (node) {
         // TODO implement here
-        return null;
+        return this;
+    };
+    /**
+     * @param connections
+     * @return
+     */
+    StoryGraph.prototype.connect = function (connections) {
+        // TODO implement here
+        return this;
+    };
+    /**
+     * @param edge
+     * @return
+     */
+    StoryGraph.prototype.disconnect = function (edge) {
+        // TODO implement here
+        return this;
+    };
+    /**
+     *
+     */
+    StoryGraph.makeContentObject = function () {
+        // TODO implement here
     };
     /**
      * @param nodes
      * @param edges
      * @return
      */
-    StoryGraph.prototype.makeGraph = function (nodes, edges) {
+    StoryGraph.makeGraph = function (nodes, edges) {
         // TODO implement here
-        return null;
+        return new this(nodes, edges);
     };
     /**
-     * @param graphx
-     * @param graphy
      * @return
      */
-    StoryGraph.prototype.merge = function (graphx, graphy) {
+    StoryGraph.makeStoryObject = function () {
         // TODO implement here
-        return null;
+        return new this();
     };
     /**
      * @param graph
      * @return
      */
-    StoryGraph.prototype.flatten = function (graph) {
+    StoryGraph.prototype.merge = function (graph) {
         // TODO implement here
-        return null;
+        return this;
+    };
+    /**
+     * @param node
+     * @return
+     */
+    StoryGraph.prototype.removeNode = function (node) {
+        // TODO implement here
+        return this;
+    };
+    /**
+     * @return
+     */
+    StoryGraph.prototype.flatten = function () {
+        // TODO implement here
+        return this;
+    };
+    /**
+     * Traverses the given graph and its subgraph and returns all nodes which match the query.
+     * @param predicate Object with parameters to match the graph's nodes against.
+     * @return Array of nodes.
+     */
+    StoryGraph.prototype.getNode = function (predicate) {
+        // TODO implement here
+        return [];
+    };
+    /**
+     * Traverses the given graph and its subgraphs and returns all matching edges.
+     * @param predicate
+     * @return
+     */
+    StoryGraph.prototype.getEdge = function (predicate) {
+        // TODO implement here
+        return [];
+    };
+    /**
+     *
+     */
+    StoryGraph.prototype.getEdgeType = function () {
+        // TODO implement here
+    };
+    /**
+     *
+     */
+    StoryGraph.prototype.getEdgeConditions = function () {
+        // TODO implement here
+    };
+    /**
+     * @param edge
+     * @param parameters
+     * @return
+     */
+    StoryGraph.prototype.setEdgeParamters = function (edge, parameters) {
+        // TODO implement here
+        return this;
+    };
+    /**
+     * @param edge
+     * @return
+     */
+    StoryGraph.prototype.setEdgeType = function (edge) {
+        // TODO implement here
+        return this;
+    };
+    /**
+     * @param node
+     * @param paramters
+     * @return
+     */
+    StoryGraph.prototype.setNodeParameters = function (node, paramters) {
+        // TODO implement here
+        return this;
+    };
+    /**
+     * @return
+     */
+    StoryGraph.prototype.toJSON = function () {
+        // TODO implement here
+        return "";
+    };
+    /**
+     * @param graph
+     * @return
+     */
+    StoryGraph.prototype.fromJSON = function (graph) {
+        // TODO implement here
+        return "";
     };
     return StoryGraph;
 }());

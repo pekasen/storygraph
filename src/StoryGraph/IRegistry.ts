d18adab954ee77944e5552161300459294abbf57
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-09 15:10:46 +0100

    add registry interface

---
+++ src/StoryGraph/IRegistry.ts
@@ -0,0 +1,8 @@
+import { IStoryObject } from "./IStoryObject";
+
+export interface IRegistry {
+    register(value: IStoryObject): boolean
+    deregister(value: string): boolean
+    getValue(forId: string): IStoryObject | undefined
+    overwrite(value: IStoryObject): boolean
+}

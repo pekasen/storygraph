ae59bb2b7e590a66c54427182a1a2b279567263a
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-09 15:06:00 +0100

    uncluter edge API

--- src/StoryGraph/IEdge.ts
+++ src/StoryGraph/IEdge.ts
@@ -1,29 +1,28 @@
 /**P. Kessling *Hamburg, September 2020*/
 import { EdgeType } from "./EdgeType"
 import { IEdgeCondition } from "./IEdgeCondition"
-import { IStoryObject } from "./IStoryObject"
 import { StoryGraph } from "./StoryGraph"
 /**
  * 
  */
 export interface IEdge {
 
+    // /**
+    //  * 
+    //  */
+    // type: EdgeType;
+    // /**
+    //  * 
+    //  */
+    // conditions: IEdgeCondition[];
     /**
      * 
      */
-    type: EdgeType;
+    from: string;
     /**
      * 
      */
-    conditions: IEdgeCondition[];
-    /**
-     * 
-     */
-    from: IStoryObject;
-    /**
-     * 
-     */
-    to: IStoryObject;
+    to: string;
     /**
      * 
      */

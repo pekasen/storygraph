85d22fb072fad8dab03bf126fc339063b1d64a41
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-16 15:37:45 +0100

    build exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,8 @@
 import { StoryGraph } from "./StoryGraph/StoryGraph";
 import { IStoryObject } from "./StoryGraph/IStoryObject";
-export { StoryGraph, IStoryObject };
+import { IEdge } from './StoryGraph/IEdge';
+import { IConnectorPort } from './StoryGraph/IConnectorPort';
+import { IMetaData } from './StoryGraph/IMetaData';
+import { IRenderingProperties } from './StoryGraph/IRenderingProperties';
+import { IStoryModifier } from './StoryGraph/IStoryModifier';
+export { StoryGraph, IStoryObject, IEdge, IStoryModifier, IMetaData, IRenderingProperties, IConnectorPort };

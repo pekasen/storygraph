7cb7b39a4bdedd93bb19ec5b2e1d78b51c983ce6
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-23 10:53:20 +0100

    update connectors with callbacks

--- dist/StoryGraph/IConnectorPort.d.ts
+++ dist/StoryGraph/IConnectorPort.d.ts
@@ -2,4 +2,5 @@ export interface IConnectorPort {
     name: string;
     direction: "in" | "out";
     type: "flow" | "reaction" | "data";
+    call?: (data: any) => void;
 }
--- src/StoryGraph/IConnectorPort.ts
+++ src/StoryGraph/IConnectorPort.ts
@@ -3,4 +3,5 @@ export interface IConnectorPort {
     name: string
     direction: "in" | "out";
     type: "flow" | "reaction" | "data";
+    call?: (data: any) => void;
 }

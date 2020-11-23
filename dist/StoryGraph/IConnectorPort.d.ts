80344571f08b1ea49a6eab885b4f98a8fae32c93
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2020-11-23 12:12:25 +0100

    fix: data callback should have optional args

--- dist/StoryGraph/IConnectorPort.d.ts
+++ dist/StoryGraph/IConnectorPort.d.ts
@@ -2,5 +2,5 @@ export interface IConnectorPort {
     name: string;
     direction: "in" | "out";
     type: "flow" | "reaction" | "data";
-    call?: (data: any) => void;
+    call?: (data?: any) => void;
 }
--- src/StoryGraph/IConnectorPort.ts
+++ src/StoryGraph/IConnectorPort.ts
@@ -3,5 +3,5 @@ export interface IConnectorPort {
     name: string
     direction: "in" | "out";
     type: "flow" | "reaction" | "data";
-    call?: (data: any) => void;
+    call?: (data?: any) => void;
 }
